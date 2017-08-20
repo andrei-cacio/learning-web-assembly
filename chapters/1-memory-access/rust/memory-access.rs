d7e774c414706a052ff5f54ced609555370f711a
Andrei Cacio <andrei.cacio@yahoo.com>
2017-08-20 21:09:21 +0300

    Added rust example off accessing an Integer array by pointer

--- chapters/1-memory-access/rust/memory-access.rs
+++ chapters/1-memory-access/rust/memory-access.rs
@@ -1,4 +1,11 @@
 #[no_mangle]
+pub extern fn get_offset() -> *mut i32 {
+	let vec = &mut [1, 2, 3, 4];
+
+	return vec.as_mut_ptr();
+}
+
+#[no_mangle]
 pub extern fn sum(a: i32, b: i32) -> i32 {
 	a + b
 }
